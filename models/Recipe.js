51e4b073beccb761ed1499c6ad45ce2c523a3d40
luciaarnaldo <luciaarnaldo01@gmail.com>
2025-12-22 10:53:13 +0100

    complete structure and data recipes.json

---
+++ models/Recipe.js
@@ -0,0 +1,11 @@
+const Recipe = {
+  id: String,
+  name: String,
+  difficulty: String,
+  time: String,
+  type: String,
+  ingredients: [String],
+  preparation: String
+};
+
+export default Recipe;
---
+++ models/User.js
@@ -0,0 +1,8 @@
+const User = {
+  id: String,
+  username: String,
+  email: String,
+  password: String
+};
+
+export default User;
